0e10313be8c815654e213d14bd51846b9c16d51a
pietroline <pietrobarberi@hotmail.it>
2022-04-06 15:18:53 +0200

    admin Routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware("auth")
+    ->namespace("Admin")
+    ->name("admin.")
+    ->prefix("admin")
+    ->group(function(){
+        Route::get('/home', 'HomeController@index')->name('home');
+    }
+);
+
